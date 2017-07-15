3c8611947bfc6bf7c91299bc3cd3e01949e909bf
Martin Wimpress <code@flexion.org>
2017-07-15 23:54:59 +0100

    Update Depends: to require libvalapanel0-dev.

--- debian/vala-panel-appmenu/debian/vala-panel-appmenu.sh
+++ debian/vala-panel-appmenu/debian/vala-panel-appmenu.sh
@@ -1,8 +1,7 @@
-# This file is sourced by Xsession(5), not executed.
-
-if [ -z "$GTK_MODULES" ] ; then
+# Load the unity-gtk-module, required by vala-panel-appmenu
+if [ -z "$GTK_MODULES" ]; then
     GTK_MODULES="unity-gtk-module"
-else    
+else
     GTK_MODULES="$GTK_MODULES:unity-gtk-module"
 fi
 export GTK_MODULES
